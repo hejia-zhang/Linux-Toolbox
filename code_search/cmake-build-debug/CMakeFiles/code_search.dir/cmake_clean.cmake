d74f1aebc8d8c1e9bf3c68af0acfa85f75eefa7c
Heikki Cheung <hjzh578@gmail.com>
2017-10-20 13:35:37 +0800

    Add new project code_search

---
+++ code_search/cmake-build-debug/CMakeFiles/code_search.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/code_search.dir/main.c.o"
+  "code_search.pdb"
+  "code_search"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/code_search.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
